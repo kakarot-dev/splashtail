c79727731fb67dee51e65442584980c3e6254411
a <a@a.com>
2024-11-07 19:31:44 +0000

    add template_shop_public_list

--- core/rust.bot_modules.core/src/lib.rs
+++ core/rust.bot_modules.core/src/lib.rs
@@ -125,6 +125,7 @@ impl silverpelt::module::Module for Module {
             (*settings::GUILD_TEMPLATES).clone(),
             (*settings::GUILD_TEMPLATES_KV).clone(),
             (*settings::GUILD_TEMPLATE_SHOP).clone(),
+            (*settings::GUILD_TEMPLATE_SHOP_PUBLIC_LIST).clone(),
         ]
     }
 
